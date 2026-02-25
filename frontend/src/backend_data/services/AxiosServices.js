618286812588bb34a9e7142efc1846932cbd9246
prax <akurathipradeep14@gmail.com>
2026-02-25 11:09:34 +0530

    Update AxiosServices.js

--- frontend/src/backend_data/services/AxiosServices.js
+++ frontend/src/backend_data/services/AxiosServices.js
@@ -1,8 +1,25 @@
-
 import axios from "axios";
 
+const API = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
 export default class AxiosServices {
+
   post(url, data, isRequired = false, header) {
-    return axios.post(url, data, isRequired ? header : {});
+    return API.post(url, data, isRequired ? header : {});
   }
+
+  get(url, isRequired = false, header) {
+    return API.get(url, isRequired ? header : {});
+  }
+
+  put(url, data, isRequired = false, header) {
+    return API.put(url, data, isRequired ? header : {});
+  }
+
+  delete(url, isRequired = false, header) {
+    return API.delete(url, isRequired ? header : {});
+  }
+
 }
